3d2b057e24d3f4e3e318650408ffb67750eb6b92
surajmateti <47922966+surajmateti@users.noreply.github.com>
2020-03-21 19:33:06 +0530

    Update Fenwick tree.cpp

--- Fenwick tree.cpp
+++ Fenwick tree.cpp
@@ -1,13 +1,12 @@
-class fenwicktree {
-private: vi ft; 
-public: 
+struct fenwicktree {
+vi ft;  
 fenwicktree(int n) 
 { 
 	ft.assign(n + 1, 0);
 }
-int rsq(int b) 
+ll rsq(int b) 
  {
-int sum = 0; 
+ll sum = 0; 
 for (; b; b -= b&-b) sum += ft[b];
 return sum;
   }
